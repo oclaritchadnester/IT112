60715c891852340dda986efaa7efae18ff950b37
oclaritchadnester <oclarit.chadnester@gmail.com>
2026-08-16 16:24:24 +0800

    Add files via upload

---
+++ OclaritChadNester-ctrl_Chad-CP1- activities.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("===============================================\n");
+    printf("              STUDENT PROFILE                 \n");
+    printf("===============================================\n");
+
+    printf("Name: Chad Nester H. Oclarit\n");
+    printf("Course and Section: BSIT-1IT1R14\n");
+    printf("Year Level: 1st Year\n");
+
+    printf("===============================================\n");
+    printf("              FUTURE PROGRAMMER               \n");
+    printf("===============================================\n");
+
+    return 0;
+}
