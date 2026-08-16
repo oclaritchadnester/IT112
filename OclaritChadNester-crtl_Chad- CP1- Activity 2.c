28b147ad2ecbcc972de98479e3eec5e576d033af
oclaritchadnester <oclarit.chadnester@gmail.com>
2026-08-16 22:31:11 +0800

    Add files via upload

---
+++ OclaritChadNester-crtl_Chad- CP1- Activity 2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("\n");
+    printf("🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁\n");
+    printf("¥                                         ¥\n");
+    printf("¥          WELCOME, PROGRAMMER!           ¥\n");
+    printf("¥                                         ¥\n");
+    printf("¥       >>> C PROGRAMMING 1 <<<           ¥\n");
+    printf("¥                                         ¥\n");
+    printf("¥     Create |   Think  |  Improve        ¥\n");
+    printf("¥     Learn  |   Code   |   Debug         ¥\n");
+    printf("¥          Let's Start Coding!            ¥\n");
+    printf("¥                                         ¥\n");
+    printf("🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁🍁\n");
+
+    return 0;
+}
